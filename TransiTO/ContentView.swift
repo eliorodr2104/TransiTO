02b932545c8be44bcfdcbd6579acd0c223ed4240
Eliomar Rodriguez <104672885+eliorodr2104@users.noreply.github.com>
2025-09-29 17:50:36 +0200

    Update ContentView.swift
    First

--- TransiTO/ContentView.swift
+++ TransiTO/ContentView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
-    @EnvironmentObject private var arrivalsViewModel: ArrivalsViewmodel
+    @EnvironmentObject private var arrivalsViewModel  : ArrivalsViewmodel
     @EnvironmentObject private var gtfsStaticViewModel: GTFSStaticViewModel
     
     var body: some View {
